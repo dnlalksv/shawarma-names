2a45f22d21386d386569a456e446ee8edb37fd59
dnlalksv <dalekseev2003@outlook.com>
2024-02-09 17:15:21 +0300

    added pltantum to the wordlist

--- src/filter_ma.py
+++ src/filter_ma.py
@@ -9,10 +9,12 @@ for word in words:
     anas = morph.parse(word)
     for ana in anas:
         tag = ana.tag
-        if (tag.POS in {'NOUN', 'ADJ'}) and 'nomn' in tag and 'sing' in tag:
+        if 'nomn' not in tag or ('sing' not in tag and 'Pltm' not in tag):
+            continue
+        if tag.POS == 'NOUN' or (tag.POS == 'ADJF' and tag.gender == 'masc'):
             nouns.add(f"\t'{word.lower()}',")
             break
 
 new_line = '\n'
 with open('../ma.js', 'w') as fout:
-    fout.write("const NOUNS = [\n" + f"{new_line.join(nouns)}" + '\n]')
+    fout.write("const MA = [\n" + f"{new_line.join(nouns)}" + '\n]')
